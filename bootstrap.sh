6ecb879ae787467c16c76b5281b896522c70d46e
Sébastien Larivière <sebastien@lariviere.me>
2020-12-13 16:04:10 -0500

    Fix doIt

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,8 @@ git pull
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
-    
-    # Get latest version of oh-my-zsh
-    rm -rf ~/.oh-my-zsh
-    git clone git@github.com:ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+	[ ! -d ~/.oh-my-zsh ] && git clone git@github.com:ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+	[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
